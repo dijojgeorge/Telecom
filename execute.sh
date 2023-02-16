04d7cb7cd7237c1aaaa1c50ec78c36bb91144563
rahulchattopadhyay <rahul.yay@gmail.com>
2023-02-16 11:43:38 +0530

    commit

--- execute.sh
+++ execute.sh
@@ -1,5 +1,4 @@
 #!/bin/sh -xe
-
 export PATH=$PATH:/usr/local/bin
 if [ $( docker ps -a -q --filter ancestor=telecom --format="{{.ID}}" | wc -l ) -gt 0 ]; then
 	docker rm $(docker stop $(docker ps -a -q --filter ancestor=telecom --format="{{.ID}}"))
